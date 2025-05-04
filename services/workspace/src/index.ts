3d0993fd2df7360ca829122b7b3346ed881b8061
jvopinho <jvopinho@isluny.org>
2025-05-04 15:33:32 -0300

    fix: remove unnecessary things

--- services/workspace/src/index.ts
+++ services/workspace/src/index.ts
@@ -1,15 +1,9 @@
-import { env } from '@/env'
-import { CryptoAdapter } from '@/infra/cryptography/crypto-adapter'
 import { setupDatabase } from '@/main/config'
 import { setupServer } from '@/main/server'
-
-import { logger } from './logger'
 
 export async function main() {
 	await setupDatabase()
 	await setupServer()
-
-	logger.debug(new CryptoAdapter(env.PASSWORD_SECRET, env.PASSWORD_IV, 'aes-256-gcm').encrypt('password'))
 }
 
 if(require.main === module) {
